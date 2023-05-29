72a9293a33335013004530ec17d7a225dc94356f
Kiril-Pavlov <Kire_P@yahoo.com>
2023-05-29 12:09:12 +0200

    Valpetra Feedback Tasks Done

--- impressionsSlider.js
+++ impressionsSlider.js
@@ -1,95 +1,36 @@
-//Code for the slider - start
-const slider1 = document.querySelector(".slider1");
-const slides1 = document.querySelectorAll(".slide1");
-const paginationBtns1 = document.querySelectorAll(".pagination-btn1");
-const prevBtn1 = document.querySelector("#prev1");
-const nextBtn1 = document.querySelector("#next1");
+const imagesImpressions = document.querySelectorAll(".slider-image-impressions");
+const dotsImpressions = document.querySelectorAll(".slider-dot-impressions");
+let currentSlideImpressions = 0;
 
-let currentSlide1 = 0;
-
-// Initialize the slider
-updateSlider();
-// Add event listener to the pagination buttons
-paginationBtns1.forEach((button, index) => {
-  button.addEventListener("click", () => {
-    currentSlide1 = index;
-    
-    updateSlider();
-  });
-});
-
-// Add event listener to the control buttons
-
-prevBtn1.addEventListener("click", () => {
-  currentSlide1--;
-  console.log(currentSlide1);
-  updateSlider();
-});
-nextBtn1.addEventListener("click", () => {
-  currentSlide1++;
-  updateSlider();
-});
-
-function updateSlider() {
-  // update slides
-
-  slides1.forEach((slide, index) => {
-    slide.classList.remove("active");
-
-    if (index === currentSlide1) {
-      slide.classList.add("active");
-    }
-  });
-  // Wait for the transition to complete before updating opacity
-  
-
-  // Update pagination buttons
-
-  paginationBtns1.forEach((button, index) => {
-    button.classList.remove("active");
-
-    if (index === currentSlide1) {
-      button.classList.add("active");
-    }
-  });
-  // Update control buttons
-
-  if (currentSlide1 === 0) {
-    prevBtn1.disabled = true;
-  } else {
-    prevBtn1.disabled = false;
+function showSlide(slideIndex) {
+  if (slideIndex < 0) {
+    slideIndex = imagesImpressions.length - 1;
+  } else if (slideIndex >= imagesImpressions.length) {
+    slideIndex = 0;
   }
-  if (currentSlide1 === slides1.length - 1) {
-    nextBtn1.disabled = true;
-  } else {
-    nextBtn1.disabled = false;
-  }
+
+  imagesImpressions.forEach((image) => {
+    image.classList.remove("active");
+  });
+
+  dotsImpressions.forEach((dot) => {
+    dot.classList.remove("active");
+  });
+
+  imagesImpressions[slideIndex].classList.add("active");
+  dotsImpressions[slideIndex].classList.add("active");
+
+  currentSlideImpressions = slideIndex;
 }
 
-
-showSlides1();
-
-function showSlides1() {
-  let i;
-  for (i = 0; i < slides1.length; i++) {
-    slides1[i].style.display = "none";
-  }
-  currentSlide1++;
-  if (currentSlide1 > slides1.length) {
-    currentSlide1 = 1;
-  }
-  for (i = 0; i < paginationBtns1.length; i++) {
-    paginationBtns1[i].className = paginationBtns1[i].className.replace(" active", "");
-  }
-  slides1[currentSlide1 - 1].style.display = "block";
-  paginationBtns1[currentSlide1 - 1].className += " active";
-  console.log(currentSlide1);
-  setTimeout(() => {
-    slides1.forEach((slide, index) => {
-      console.log("here",index, currentSlide1);
-      slide.style.opacity = index === currentSlide1-1 ? '1' : '0';
-    });
-  }, 100);
-  setTimeout(showSlides1, 4000); // Change image every 2 seconds
+function nextSlide() {
+  showSlide(currentSlideImpressions + 1);
 }
-//Code for the slider - end
+
+setInterval(nextSlide, 4000);
+
+dotsImpressions.forEach((dot, index) => {
+  dot.addEventListener("click", () => {
+    showSlide(index);
+  });
+});
--- index.js
+++ index.js
@@ -3,14 +3,15 @@
 function updateHeader() {
   var scrollValue = window.scrollY;
   var header = document.getElementById("header");
-  var verbier = document.getElementById("verbier");
+  // var verbier = document.getElementById("verbier");
   var line = document.getElementById("line");
+  console.log(line);
   var download = document.getElementById("download");
   var lines = document.querySelectorAll(".menu-btn-lines");
 
   if (scrollValue < 500) {
     header.classList.remove("header-background-change");
-    verbier.classList.remove("header-background-change");
+    // verbier.classList.remove("header-background-change");
     line.classList.remove("header-background-color-change");
     download.classList.remove("header-border-color-change");
     lines.forEach((line) => {
@@ -18,7 +19,7 @@ function updateHeader() {
     });
   } else {
     header.classList.add("header-background-change");
-    verbier.classList.add("header-background-change");
+    // verbier.classList.add("header-background-change");
     line.classList.add("header-background-color-change");
     download.classList.add("header-border-color-change");
     lines.forEach((line) => {
@@ -126,98 +127,96 @@ function toggleInsideTabActive() {
 
 
 
-//Code for the slider - start
-const slider = document.querySelector(".slider");
-const slides = document.querySelectorAll(".slide");
-const paginationBtns = document.querySelectorAll(".pagination-btn");
-const prevBtn = document.querySelector("#prev");
-const nextBtn = document.querySelector("#next");
+// //Code for the slider - start
+// const slider = document.querySelector(".slider");
+// const slides = document.querySelectorAll(".slide");
+// const paginationBtns = document.querySelectorAll(".pagination-btn");
+// const prevBtn = document.querySelector("#prev");
+// const nextBtn = document.querySelector("#next");
 
-let currentSlide = 0;
+// let currentSlide = 0;
 
-// Initialize the slider
-updateSlider();
-// Add event listener to the pagination buttons
-paginationBtns.forEach((button, index) => {
-  button.addEventListener("click", () => {
-    currentSlide = index;
+// // Initialize the slider
+// updateSlider();
+// // Add event listener to the pagination buttons
+// paginationBtns.forEach((button, index) => {
+//   button.addEventListener("click", () => {
+//     currentSlide = index;
 
-    updateSlider();
-  });
-});
+//     updateSlider();
+//   });
+// });
 
-// Add event listener to the control buttons
+// // Add event listener to the control buttons
 
-prevBtn.addEventListener("click", () => {
-  currentSlide--;
-  updateSlider();
-});
-nextBtn.addEventListener("click", () => {
-  currentSlide++;
-  updateSlider();
-});
+// prevBtn.addEventListener("click", () => {
+//   currentSlide--;
+//   updateSlider();
+// });
+// nextBtn.addEventListener("click", () => {
+//   currentSlide++;
+//   updateSlider();
+// });
 
-function updateSlider() {
-  // update slides
+// function updateSlider() {
+//   // update slides
 
-  slides.forEach((slide, index) => {
-    slide.classList.remove("active");
+//   slides.forEach((slide, index) => {
+//     slide.classList.remove("active");
 
-    if (index === currentSlide) {
-      slide.classList.add("active");
-    }
-  });
-  // Update pagination buttons
+//     if (index === currentSlide) {
+//       slide.classList.add("active");
+//     }
+//   });
+//   // Update pagination buttons
 
-  paginationBtns.forEach((button, index) => {
-    button.classList.remove("active");
+//   paginationBtns.forEach((button, index) => {
+//     button.classList.remove("active");
 
-    if (index === currentSlide) {
-      button.classList.add("active");
-    }
-  });
-  // Update control buttons
+//     if (index === currentSlide) {
+//       button.classList.add("active");
+//     }
+//   });
+//   // Update control buttons
 
-  if (currentSlide === 0) {
-    prevBtn.disabled = true;
-  } else {
-    prevBtn.disabled = false;
-  }
-  if (currentSlide === slides.length - 1) {
-    nextBtn.disabled = true;
-  } else {
-    nextBtn.disabled = false;
-  }
-}
-
-
-showSlides();
-
-function showSlides() {
-  let i;
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  currentSlide++;
-  if (currentSlide > slides.length) {
-    currentSlide = 1;
-  }
-  for (i = 0; i < paginationBtns.length; i++) {
-    paginationBtns[i].className = paginationBtns[i].className.replace(" active", "");
-  }
-  slides[currentSlide - 1].style.display = "block";
-  paginationBtns[currentSlide - 1].className += " active";
-  setTimeout(() => {
-    slides.forEach((slide, index) => {
-      console.log("here",index, currentSlide);
-      slide.style.opacity = index === currentSlide-1 ? '1' : '0';
-    });
-  }, 100);
-  setTimeout(showSlides, 4000); // Change image every 2 seconds
-}
-//Code for the slider - end
+//   if (currentSlide === 0) {
+//     prevBtn.disabled = true;
+//   } else {
+//     prevBtn.disabled = false;
+//   }
+//   if (currentSlide === slides.length - 1) {
+//     nextBtn.disabled = true;
+//   } else {
+//     nextBtn.disabled = false;
+//   }
+// }
 
 
+// showSlides();
+
+// function showSlides() {
+//   let i;
+//   for (i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none";
+//   }
+//   currentSlide++;
+//   if (currentSlide > slides.length) {
+//     currentSlide = 1;
+//   }
+//   for (i = 0; i < paginationBtns.length; i++) {
+//     paginationBtns[i].className = paginationBtns[i].className.replace(" active", "");
+//   }
+//   slides[currentSlide - 1].style.display = "block";
+//   paginationBtns[currentSlide - 1].className += " active";
+//   setTimeout(() => {
+//     slides.forEach((slide, index) => {
+//       // console.log("here",index, currentSlide);
+//       slide.style.opacity = index === currentSlide-1 ? '1' : '0';
+//     });
+//   }, 100);
+//   setTimeout(showSlides, 4000); // Change image every 2 seconds
+// }
+// //Code for the slider - end
 
 
 
@@ -229,7 +228,9 @@ function showSlides() {
 
 
 
-// Code for versatility animation - start
+
+
+// // Code for versatility animation - start
 
 window.addEventListener("scroll", () => {
   var pageY = window.pageYOffset;
@@ -282,4 +283,137 @@ gsap.to(".subhero_section", {
     start: "top 95%",
     end: "bottom top",
   },
+});
+
+// //Code for the slider - start
+// const slider1 = document.querySelector(".slider1");
+// const slides1 = document.querySelectorAll(".slide1");
+// const paginationBtns1 = document.querySelectorAll(".pagination-btn1");
+// const prevBtn1 = document.querySelector("#prev1");
+// const nextBtn1 = document.querySelector("#next1");
+
+// let currentSlide1 = 0;
+
+// // Initialize the slider
+// updateSlider();
+// // Add event listener to the pagination buttons
+// paginationBtns1.forEach((button, index) => {
+//   button.addEventListener("click", () => {
+//     currentSlide1 = index;
+    
+//     updateSlider();
+//   });
+// });
+
+// // Add event listener to the control buttons
+
+// prevBtn1.addEventListener("click", () => {
+//   currentSlide1--;
+//   console.log(currentSlide1);
+//   updateSlider();
+// });
+// nextBtn1.addEventListener("click", () => {
+//   currentSlide1++;
+//   updateSlider();
+// });
+
+// function updateSlider() {
+//   // update slides
+
+//   slides1.forEach((slide, index) => {
+//     slide.classList.remove("active");
+
+//     if (index === currentSlide1) {
+//       slide.classList.add("active");
+//     }
+//   });
+//   // Wait for the transition to complete before updating opacity
+  
+
+//   // Update pagination buttons
+
+//   paginationBtns1.forEach((button, index) => {
+//     button.classList.remove("active");
+
+//     if (index === currentSlide1) {
+//       button.classList.add("active");
+//     }
+//   });
+//   // Update control buttons
+
+//   if (currentSlide1 === 0) {
+//     prevBtn1.disabled = true;
+//   } else {
+//     prevBtn1.disabled = false;
+//   }
+//   if (currentSlide1 === slides1.length - 1) {
+//     nextBtn1.disabled = true;
+//   } else {
+//     nextBtn1.disabled = false;
+//   }
+// }
+
+
+// showSlides1();
+
+// function showSlides1() {
+//   let i;
+//   for (i = 0; i < slides1.length; i++) {
+//     slides1[i].style.display = "none";
+//   }
+//   currentSlide1++;
+//   if (currentSlide1 > slides1.length) {
+//     currentSlide1 = 1;
+//   }
+//   for (i = 0; i < paginationBtns1.length; i++) {
+//     paginationBtns1[i].className = paginationBtns1[i].className.replace(" active", "");
+//   }
+//   slides1[currentSlide1 - 1].style.display = "block";
+//   paginationBtns1[currentSlide1 - 1].className += " active";
+//   console.log(currentSlide1);
+//   setTimeout(() => {
+//     slides1.forEach((slide, index) => {
+//       // console.log("here",index, currentSlide1);
+//       slide.style.opacity = index === currentSlide1-1 ? '1' : '0';
+//     });
+//   }, 100);
+//   setTimeout(showSlides1, 4000); // Change image every 2 seconds
+// }
+// //Code for the slider - end
+
+const imagesLayout = document.querySelectorAll(".slider-image-layout");
+const dotsLayout = document.querySelectorAll(".slider-dot-layout");
+let currentSlideLayout = 0;
+
+function showSlideLayout(slideIndex) {
+  if (slideIndex < 0) {
+    slideIndex = imagesLayout.length - 1;
+  } else if (slideIndex >= imagesLayout.length) {
+    slideIndex = 0;
+  }
+
+  imagesLayout.forEach((image) => {
+    image.classList.remove("active");
+  });
+
+  dotsLayout.forEach((dot) => {
+    dot.classList.remove("active");
+  });
+
+  imagesLayout[slideIndex].classList.add("active");
+  dotsLayout[slideIndex].classList.add("active");
+
+  currentSlideLayout = slideIndex;
+}
+
+function nextSlideLayout() {
+  showSlideLayout(currentSlideLayout + 1);
+}
+
+setInterval(nextSlideLayout, 4000);
+
+dotsLayout.forEach((dot, index) => {
+  dot.addEventListener("click", () => {
+    showSlideLayout(index);
+  });
 });
